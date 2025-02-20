6ef7f01aff8052b527a3cbf4f165975c96172c75
MuhammadMaveed <maveedshawal@gmail.com>
2025-02-20 12:51:05 +0500

    Shared Preference App Flow is done

--- lib/main.dart
+++ lib/main.dart
@@ -25,9 +25,9 @@ Future<void> main() async {
       webProvider: ReCaptchaV3Provider('PASTE_YOUR_RECAPTCHA_SITE_KEY_HERE'),
     );
 
-    debugPrint(" Firebase initialized successfully.");
+    debugPrint("  Firebase initialized successfully.");
   } catch (e) {
-    debugPrint(" Firebase Initialization Error: $e");
+    debugPrint("  Firebase Initialization Error: $e");
   }
 
   Get.put(TranslationsController());
