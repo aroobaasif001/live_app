f9f008317a7c4389a11ec547122193f432ef24b7
Soban Qazi <111500255+sobanqazi69@users.noreply.github.com>
2025-03-24 11:35:20 +0500

    updatre new codeok

--- lib/custom_widgets/custom_textfield.dart
+++ lib/custom_widgets/custom_textfield.dart
@@ -16,43 +16,36 @@ class CustomTextField extends StatelessWidget {
     this.controller,
     this.isPassword = false,
     this.obscureText = false,
-    this.validator, this.suffixIcon,this.prefixIcon,
+    this.validator,
+    this.suffixIcon,
+    this.prefixIcon,
     this.keyboardType,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 55,
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.05),
-            blurRadius: 10,
-            spreadRadius: 2,
-            offset: const Offset(0, 3),
-          ),
-        ],
-      ),
-      child: TextFormField(
-        controller: controller,
-        obscureText: obscureText,
-        validator: validator,
-        keyboardType: keyboardType,
-        decoration: InputDecoration(
-          border: InputBorder.none, // ✅ No visible borders
-          hintText: hintText,
-          hintStyle: const TextStyle(
-            fontWeight: FontWeight.bold,
-            color: Colors.grey, // ✅ Matches label color
-            fontSize: 16,
-          ),
-          suffixIcon: suffixIcon, // ✅ No suffix icon for non-password fields
-          prefixIcon: prefixIcon, // ✅ No suffix icon for non-password fields
+    return TextFormField(
+      controller: controller,
+      obscureText: obscureText,
+      validator: validator,
+      keyboardType: keyboardType,
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Colors.white, // Background color
+        contentPadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10), // Rounded corners
+          borderSide: BorderSide.none, // No visible border
         ),
+        hintText: hintText,
+        hintStyle: const TextStyle(
+          fontWeight: FontWeight.bold,
+          color: Colors.grey,
+          fontSize: 16,
+        ),
+        suffixIcon: suffixIcon,
+        prefixIcon: prefixIcon,
+
       ),
     );
   }
